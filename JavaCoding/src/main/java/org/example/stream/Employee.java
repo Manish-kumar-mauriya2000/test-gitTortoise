3bed49c0909174f4267df06ed215b473ee8855b3
Manish Kumar Mauriya <manishkumarmauriya2000@gmail.com>
2026-03-19 13:49:02 +0530

    Employee class added

---
+++ JavaCoding/src/main/java/org/example/stream/Employee.java
@@ -0,0 +1,41 @@
+package org.example.stream;
+
+
+public class Employee {
+    private int employeeId;
+    private String employeeName;
+    private String employeeDept;
+    private double salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeDept() {
+        return employeeDept;
+    }
+
+    public void setEmployeeDept(String employeeDept) {
+        this.employeeDept = employeeDept;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
